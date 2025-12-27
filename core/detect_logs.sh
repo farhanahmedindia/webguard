08f75cb869d0fd95ed8a53f1b37db823da1940f6
Farhan Ahmed <80202023+farhanahmedindia@users.noreply.github.com>
2025-12-27 15:45:43 +0530

    Update detect_logs.sh

--- core/detect_logs.sh
+++ core/detect_logs.sh
@@ -1,3 +1,7 @@
+# Copyright 2025 Farhan Ahmed
+# Licensed under the Apache License, Version 2.0
+# See LICENSE file for details
+
 APACHE_DIRS=("/var/log/apache2" "/var/log/httpd")
 
 for d in "${APACHE_DIRS[@]}"; do
