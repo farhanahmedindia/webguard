41d293302cc9c5b88fa9a666e84fdfb0851fceec
Farhan Ahmed <80202023+farhanahmedindia@users.noreply.github.com>
2025-12-27 14:27:33 +0530

    Create detect_logs.sh

---
+++ detect_logs.sh
@@ -0,0 +1,10 @@
+APACHE_DIRS=("/var/log/apache2" "/var/log/httpd")
+
+for d in "${APACHE_DIRS[@]}"; do
+  if ls $d/*access.log &>/dev/null; then
+    LOG_DIR="$d"
+    break
+  fi
+done
+
+[[ -z "$LOG_DIR" ]] && exit 1
