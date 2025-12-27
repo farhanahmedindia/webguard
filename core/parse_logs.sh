8ea90cc7dc2e7f34a84222b80227caaa32d7381f
Farhan Ahmed <80202023+farhanahmedindia@users.noreply.github.com>
2025-12-27 17:53:16 +0530

    Update parse_logs.sh

--- core/parse_logs.sh
+++ core/parse_logs.sh
@@ -1,11 +1,36 @@
+#!/bin/bash
 # Copyright 2025 Farhan Ahmed
 # Licensed under the Apache License, Version 2.0
 # See LICENSE file for details
 
-NOW=$(date +"%d/%b/%Y:%H:%M")
 TMP="/tmp/webguard_hits.tmp"
 > "$TMP"
 
+NOW_EPOCH=$(date +%s)
+WINDOW=120   # seconds (2 minutes)
+
 for f in "$LOG_DIR"/*access.log; do
-  awk -v t="$NOW" '$4 ~ t {print $1}' "$f"
+  awk -v now="$NOW_EPOCH" -v win="$WINDOW" '
+  {
+    # Remove leading [
+    gsub(/\[/, "", $4)
+
+    # Split Apache timestamp
+    split($4, t, ":")
+    split(t[1], d, "/")
+
+    # Month conversion
+    month = "JanFebMarAprMayJunJulAugSepOctNovDec"
+    mon = (index(month, d[2]) + 2) / 3
+
+    # Convert to epoch
+    ts = mktime(d[3]" "mon" "d[1]" "t[2]" "t[3]" "t[4])
+
+    # Sliding window check
+    if (now - ts <= win)
+      print $1
+  }
+  ' "$f"
 done | sort | uniq -c > "$TMP"
+
+export TMP_HITS="$TMP"
