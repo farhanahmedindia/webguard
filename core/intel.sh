d5d79451da1f33b65a5e35a80d343c1cc883f874
Farhan Ahmed <80202023+farhanahmedindia@users.noreply.github.com>
2025-12-28 11:04:49 +0530

    Update intel.sh

--- core/intel.sh
+++ core/intel.sh
@@ -5,3 +5,9 @@
 ip_info() {
   whois "$1" | grep -Ei "OrgName|Country|origin"
 }
+
+intel_lookup() {
+  IP="$1"
+  echo "IP Intelligence for $IP"
+  whois "$IP" | grep -Ei 'country|org|netname|descr|origin' | head -n 15
+}
